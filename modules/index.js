7271d098ac94448f84bad41996d299fa52ac3fcf
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-07-26 01:14:32 +0400

    add megafon sources

--- modules/index.js
+++ modules/index.js
@@ -2,11 +2,15 @@ import { lonLatToPixel, meterToPixel } from './conversions'
 import * as tileSources from './tileSources'
 import coverage from './coverage'
 
-
 const mts2G = (zoom) => coverage(tileSources.mts2G, zoom)
 const mts3G = (zoom) => coverage(tileSources.mts3G, zoom)
 const mts4G = (zoom) => coverage(tileSources.mts4G, zoom)
 const mts4GPlan = (zoom) => coverage(tileSources.mts4GPlan, zoom)
+
+const megafon2G = (zoom) => coverage(tileSources.megafon2G, zoom)
+const megafon3G = (zoom) => coverage(tileSources.megafon3G, zoom)
+const megafon4G = (zoom) => coverage(tileSources.megafon4G, zoom)
+const megafon4GPlus = (zoom) => coverage(tileSources.megafon4GPlus, zoom)
 
 export {
   lonLatToPixel,
@@ -14,5 +18,9 @@ export {
   mts2G,
   mts3G,
   mts4G,
-  mts4GPlan
+  mts4GPlan,
+  megafon2G,
+  megafon3G,
+  megafon4G,
+  megafon4GPlus
 }
--- modules/tileSources.js
+++ modules/tileSources.js
@@ -2,3 +2,9 @@ export const mts2G = (z, x, y) => `http://tiles.mts.ru/G2_New/${z}/${x}/${y}/`
 export const mts3G = (z, x, y) => `http://tiles.mts.ru/G3_New/${z}/${x}/${y}/`
 export const mts4G = (z, x, y) => `http://tiles.mts.ru/LTE_New/${z}/${x}/${y}/`
 export const mts4GPlan = (z, x, y) => `http://tiles.mts.ru/LTE_Plan_New/${z}/${x}/${y}/`
+
+// 2G map uses hatching for zones with no signal and transparent pixels for zone where signal present.
+export const megafon2G = (z, x, y) => `http://yamap.megafon.ru/g2/${z}/${x}/${y}/`
+export const megafon3G = (z, x, y) => `http://yamap.megafon.ru/g3/${z}/${x}/${y}/`
+export const megafon4G = (z, x, y) => `http://yamap.megafon.ru/lte/${z}/${x}/${y}/`
+export const megafon4GPlus = (z, x, y) => `http://yamap.megafon.ru/lte_plus/${z}/${x}/${y}/`
